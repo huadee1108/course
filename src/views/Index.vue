1fd6761b663a8f95b31ec749b6c5015ffab4dcf5
huadee1108 <981929654@qq.com>
2019-02-15 13:23:47 +0800

    first submit

---
+++ src/views/Index.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="all">
+        <div class="log">
+            <img class="log_left" src="../assets/log.png">
+            <img class="log_right" :src="photoImg">
+        </div>
+        <hr>
+        <div class="course_list">
+            <img class="course_img" src="../assets/jj.jpg">
+            <div class="num">
+                100人报名
+            </div>
+            <h4>步步高教育</h4>
+            <h5>步步高升</h5>
+        </div>
+        <div class="course_list">
+            <img class="course_img" src="../assets/jj.jpg">
+            <div class="num">
+                100人报名
+            </div>
+            <h4>步步高教育</h4>
+            <h5>步步高升</h5>
+        </div>
+        <div class="course_list">
+            <img class="course_img" src="../assets/jj.jpg">
+            <div class="num">
+                100人报名
+            </div>
+            <h4>步步高教育</h4>
+            <h5>步步高升</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+        data(){
+            return{
+                photoImg: ''
+            }
+        },
+        created:function () {
+            this.photoImg = localStorage.getItem('photoImg');
+            console.log(this.photoImg);
+        }
+    }
+</script>
+
+<style scoped>
+.all{
+    width: 100%;
+}
+.log{
+    width: 90%;
+    height: 26px;
+    margin: 10px auto;
+}
+.log_left{
+    float: left;
+}
+.log_right{
+    float: right;
+}
+.course_list{
+    position: relative;
+    width: 90%;
+    margin: 10px auto;
+}
+.course_img{
+    width: 100%;
+    height: 220px;
+    position: relative;
+}
+.num{
+    width: 100%;
+    height: 40px;
+    background: black;
+    opacity: 0.3;
+    position: absolute;
+    top: 180px;
+    color: white;
+    text-indent: 1rem;
+    line-height: 40px;
+}
+h4{
+    margin-top: 10px;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="container">
+        <h4 class="login_text">登录</h4>
+        <div class="form-group">
+            <input type="text" class="form-control" id="username" placeholder="请输入用户名/学员编号">
+            <img src="../assets/username.png">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="password" placeholder="请输入密码">
+            <img src="../assets/password.png">
+            <p v-show="status" class="tips">{{errorMessage}}</p>
+        </div>
+        <button @click="goIndex" type="button" class="btn btn-success btn-block">登录</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return{
+                status: false,
+                loginMessage:{
+                    username: '',
+                    password: ''
+                },
+                errorMessage: '',
+                photoImg: '../assets/log.png'
+            }
+        },
+        methods:{
+            goIndex: function () {
+                /*this.$axios.get('http://192.168.23.10/login/token.php?',
+                    this.loginMessage)
+                    .then((response) => {
+                        console.log(response);
+                        this.photoImg = response.img;
+                        this.$router.push({path:'index',params:{photoImg:this.photoImg}});
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                        this.status = true;
+                        this.errorMessage = "dd";
+                    })*/
+                localStorage.setItem('photoImg',this.photoImg);
+                this.$router.push({path:'index'});
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.login_text{
+    text-align: center;
+    margin-top: 12px;
+}
+.form-group{
+    position: relative;
+}
+.form-group img{
+    position: absolute;
+    left: 5px;
+    top: 10px;
+}
+.form-group input{
+    text-indent: 1em;
+}
+.tips{
+    color: red;
+}
+</style>
